3f38f05ce8c7db63340f66d953952827155889bb
Haruya Ishikawa <Toraudonn@users.noreply.github.com>
2017-12-02 13:27:14 +0900

    create outer cell coordinates

---
+++ create_outer_cell_coordinates.m
@@ -0,0 +1,30 @@
+function coordinates = create_outer_cell_coordinates(  )
+% Create outer macro cell coordinates 
+
+is_d = 500;     % inter-site distance (500m)
+
+coordinates = zeros(6, 7);  
+r = (is_d/2) / cos( pi/6 );    % cell radius
+
+% coordinates counter-clock wise from top left
+origin_points = [ -3*r + 2*is_d*1i, ...
+                  -(9/2)*r - (1/2)*is_d*1i, ...
+                  -(3/2)*r - (5/2)*is_d*1i, ...
+                  3*r - 2*is_d*1i, ...
+                  (9/2)*r + (1/2)*is_d*1i, ...
+                  (3/2)*r + (5/2)*is_d*1i];
+
+for i = 1:numel(origin_points)
+    
+    origin = origin_points( i );
+    coordinates(i, 1) = origin;
+    
+    for j = 2:7
+        coordinates(i, j) = origin + is_d*cos( j * pi/3 - pi/6 ) ...
+                             + is_d*sin( j * pi/3 - pi/6 )*1i;
+    end
+    
+end
+
+end
+
